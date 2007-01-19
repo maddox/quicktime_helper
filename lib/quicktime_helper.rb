148536542b6ac7526221ed2c61ac248a0636d1bc
Jon Maddox <jon@jonsthoughtsoneverything.com>
2007-01-19 05:43:42 +0000

    added the ability to use a placeholder image

--- lib/quicktime_helper.rb
+++ lib/quicktime_helper.rb
@@ -5,19 +5,27 @@ module QuicktimeHelper
     
     quicktime_params = ""
     
-    options.each_pair { |key, value| quicktime_params += "myQTObject.addParam(\"#{key}\", \"#{value}\");" }
-	
-  	"<script type=\"text/javascript\">
-  	// <![CDATA[
-  		var myQTObject = new QTObject(\"#{filename_with_path}\", \"#{id}\", \"#{width}\", \"#{options[:controller] == false ? height : height + 15 }\");
-  		#{quicktime_params}
-  		myQTObject.write();
-  	// ]]>
-  	</script>
-  	<noscript>
-  		<p>You must enable Javascript to view this content. <a href=\"#{filename_with_path}\">(Link to alternate content)</a></p>
-  	</noscript>
-  	"
+    if options.has_key? :placeholder
+      initializer = %{var myQTObject = new QTObject("#{options[:placeholder]}", "#{id}", "#{width}", "#{options[:controller] == false ? height : height + 15 }");}
+      options.delete_if {|key, value| key == :placeholder }
+      options.merge(:href => filename_with_path, :target => "myself").each_pair { |key, value| quicktime_params += "myQTObject.addParam(\"#{key}\", \"#{value}\");" }
+    else
+      initializer = %{var myQTObject = new QTObject("#{filename_with_path}", "#{id}", "#{width}", "#{options[:controller] == false ? height : height + 15 }");}
+      options.each_pair { |key, value| quicktime_params += "myQTObject.addParam(\"#{key}\", \"#{value}\");" }
+    end
+	  	
+  	%{
+  	  <script type="text/javascript">
+  	    // <![CDATA[
+          #{initializer}
+  		    #{quicktime_params}
+  		    myQTObject.write();
+  	    // ]]>
+  	  </script>
+  	  <noscript>
+  		  <p>You must enable Javascript to view this content. <a href="#{filename_with_path}">(Link to alternate content)</a></p>
+  	  </noscript>
+    } 
   end
 
 end
